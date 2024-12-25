1ba515d445075dd41a6ba0468eb7708c570a1bb8
Florian Philippe <florian.philippegm@gmail.com>
2024-12-25 20:50:03 +0100

    run black

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 
 def extract_test_y(language) -> list[str]:
-    lines =  open(f"../data/deft09_parlement_ref/deft09_parlement_ref_{language}.txt").readlines()
+    lines = open(
+        f"../data/deft09_parlement_ref/deft09_parlement_ref_{language}.txt"
+    ).readlines()
     return [line.split("\t")[-1].strip() for line in lines]
+
 
 def extract_texts_from_file(path: Path) -> list[dict]:
     if "appr" in path.name:
@@ -15,8 +18,10 @@ def extract_texts_from_file(path: Path) -> list[dict]:
         train = False
         split = "test"
     else:
-        raise FileNotFoundError("Cette fonction prend en entrée un fichier d'apprentissage ou de test")
-    
+        raise FileNotFoundError(
+            "Cette fonction prend en entrée un fichier d'apprentissage ou de test"
+        )
+
     docs = []
     tree = ET.parse(path)
     root = tree.getroot()
@@ -25,18 +30,18 @@ def extract_texts_from_file(path: Path) -> list[dict]:
         ys = []
     else:
         ys = extract_test_y(language)
-    for doc in root.findall('doc'):
-        doc_id = doc.get('id')
+    for doc in root.findall("doc"):
+        doc_id = doc.get("id")
         parti = doc.find(".//PARTI")
         if train:
             ys.append(parti.get("valeur"))
         paragraphs = [p.text for p in doc.findall(".//p")]
         docs.append(
             {
-                "id" : doc_id,
+                "id": doc_id,
                 "language": language,
-                "paragraphs" : paragraphs,
-                "split" : split,
+                "paragraphs": paragraphs,
+                "split": split,
             }
         )
     df = pd.DataFrame(docs)
@@ -59,13 +64,16 @@ def convert_corpus_to_dataframe(
         df = pd.concat([df, extract_texts_from_file(f)])
     return df
 
-def get_train_test(df: pd.DataFrame = convert_corpus_to_dataframe()) -> tuple[pd.Series]:
+
+def get_train_test(
+    df: pd.DataFrame = convert_corpus_to_dataframe(),
+) -> tuple[pd.Series]:
     """
     Renvoie le tuple (X_train: list[list[str]], X_test: list[list[str]], y_train: list[str] , y_test: list[str])
     """
-    X_train = df["paragraphs"][df["split"]=="train"]
-    X_test = df["paragraphs"][df["split"]=="test"]
-    y_train = df["y"][df["split"]=="train"]
-    y_test = df["y"][df["split"]=="test"]
+    X_train = df["paragraphs"][df["split"] == "train"]
+    X_test = df["paragraphs"][df["split"] == "test"]
+    y_train = df["y"][df["split"] == "train"]
+    y_test = df["y"][df["split"] == "test"]
 
     return X_train, X_test, y_train, y_test
