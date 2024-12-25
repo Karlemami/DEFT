7f2533f98a8feab9350eab3298d680b90b489312
Florian Philippe <florian.philippegm@gmail.com>
2024-12-25 20:16:43 +0100

    add get_train_test function

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,36 +1,73 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
-from typing import List, Dict
 from pathlib import Path
 
 
-def extract_texts_from_file(path: Path) -> List[Dict]:
+def extract_test_y(language) -> list[str]:
+    lines =  open(f"../data/deft09_parlement_ref/deft09_parlement_ref_{language}.txt").readlines()
+    return [line.split("\t")[-1].strip() for line in lines]
+
+def extract_texts_from_file(path: Path) -> list[dict]:
+    if "appr" in path.name:
+        train = True
+        split = "train"
+    elif "test" in path.name:
+        train = False
+        split = "test"
+    else:
+        raise FileNotFoundError("Cette fonction prend en entrée un fichier d'apprentissage ou de test")
+    
     docs = []
     tree = ET.parse(path)
     root = tree.getroot()
-
-    for doc in root.findall("doc"):
-        doc_id = doc.get("id")
-        language = doc_id.split("_")[1][:2]
+    language = path.name.split(".")[-2][-2:]
+    if train:
+        ys = []
+    else:
+        ys = extract_test_y(language)
+    for doc in root.findall('doc'):
+        doc_id = doc.get('id')
         parti = doc.find(".//PARTI")
-        nom_parti = parti.get("valeur")
+        if train:
+            ys.append(parti.get("valeur"))
         paragraphs = [p.text for p in doc.findall(".//p")]
         docs.append(
             {
-                "id": doc_id,
+                "id" : doc_id,
                 "language": language,
-                "parti": nom_parti,
-                "paragraphs": paragraphs,
+                "paragraphs" : paragraphs,
+                "split" : split,
             }
         )
-    return docs
+    df = pd.DataFrame(docs)
+    df["y"] = ys
+    return df
 
 
 def convert_corpus_to_dataframe(
-    path: str = "../data/deft09_parlement_appr",
+    train_path: str = "../data/deft09_parlement_appr",
+    test_path: str = "../data/deft09_parlement_test",
 ) -> pd.DataFrame:
-    directory = Path(path)
-    docs = []
-    for f in directory.glob("*.xml"):
-        docs.extend(extract_texts_from_file(f))
-    return pd.DataFrame(docs)
+    """
+    Renvoie le corpus (train et test) en dataframe avec le colonnes (id: str, language: str, paragraphs[list[str]], split: str, y: str)
+    """
+    train_directory = Path(train_path)
+    test_dictory = Path(test_path)
+    files = list(train_directory.glob("*.xml")) + list(test_dictory.glob("*.xml"))
+    df = pd.DataFrame()
+    for f in files:
+        df = pd.concat([df, extract_texts_from_file(f)])
+    return df
+
+def get_train_test(corpus: pd.DataFrame = convert_corpus_to_dataframe()) -> tuple[list[str]]:
+    """
+    Renvoie le tuple (X_train, X_test, y_train, y_test)
+    """
+    df = convert_corpus_to_dataframe()
+    X_train = df["paragraphs"][df["split"]=="train"]
+    X_test = df["paragraphs"][df["split"]=="test"]
+    y_train = df["y"][df["split"]=="train"]
+    y_test = df["y"][df["split"]=="test"]
+
+    return X_train, X_test, y_train, y_test
+
